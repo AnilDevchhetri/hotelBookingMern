f6c8ca34b3d5f117f7af89c328c3d42be7a68753
AnilDevchhetri <chhetrivision@gmail.com>
2025-08-15 01:29:27 +0900

    good

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes('owner');
   return (
     <div>
+
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>)
 }
 
@@ -19,4 +26,4 @@ export default App;
 //https://www.figma.com/design/vWAjka7MDKh3QWfvERF6Mx/QuickStay?node-id=0-1&p=f
 
 //https://www.youtube.com/watch?v=ubM9cX8G_gk&ab_channel=GreatStack
-//31:45
+//51:00
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Hero = () => {
+    return (
+        <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+            <p className="bg-[#49B9ff]/50 px-3.5 py-1 rounded-full mt-20">The Ultimate Hotel Experience</p>
+            <h1 className="font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">Lorem ipsum dolor sit amet Lorem, ipsum.</h1>
+            <p className="max-w-130 mt-2 text-sm md:text-base">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa aut ipsam corporis doloremque praesentium repellat atque unde nam vero reprehenderit?</p>
+        </div>
+    )
+}
+export default Hero;
